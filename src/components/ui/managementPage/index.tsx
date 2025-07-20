b44b42bd653e0a558136eea8358ed508571ae2fe
Wichtra <willy.chairullah@gmail.com>
2025-07-20 22:18:51 +0700

    Remove CategoryManagementContent export from management page index

--- src/components/ui/managementPage/index.tsx
+++ src/components/ui/managementPage/index.tsx
@@ -2,5 +2,3 @@ export { default as ManagementTableContent } from "./ManagementTableContent";
 export { default as ManagementContent } from "./ManagementContent";
 export { default as ManagementHeader } from "./ManagementHeader";
 export { default as ManagementForm } from "./ManagementForm";
-export { default as CategoryManagementContent } from "./CategoryManagementContent";
-
